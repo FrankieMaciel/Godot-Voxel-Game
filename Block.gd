4a0b92abaefcb04f2ab7ab6634eb84d10a4bcc58
Frankie <110301698+FrankieMaciel@users.noreply.github.com>
2025-04-22 10:11:13 -0300

    texture fix 2

--- Block.gd
+++ Block.gd
@@ -8,7 +8,7 @@ const blocks = [
 	"Grass_Side"
 ]
 const textures = [
-	"res://textures/Stone.png",
+	"res://textures/Dirt.png",
 	"res://textures/Stone.png",
 	"res://textures/Dirt.png",
 	"res://textures/Grass.png",
@@ -22,16 +22,23 @@ var ids_to_texture = {}
 var ids = {}
 
 func _ready() -> void:
+	
+	texture_array = ResourceLoader.load("res://texture_2d_array.res")
+	var hasToCreate = false
+	if (texture_array.get_layers() == 0): hasToCreate = true
+	
 	for index in range(0, len(blocks)):
 		ids[blocks[index]] = index 
 		ids_to_texture[index] = textures[index]
-		var img = Image.new()
-		img.load(textures[index])
-		images_array.append(img)
+		if (hasToCreate):
+			var img = Image.new()
+			img.load(textures[index])
+			images_array.append(img)
 	
-	print(images_array)
-	texture_array.create_from_images(images_array)
-	ResourceSaver.save(texture_array, "res://texture_2d_array.res", ResourceSaver.FLAG_COMPRESS)
+	print(texture_array.get_layers())
+	if (hasToCreate):
+		texture_array.create_from_images(images_array)
+		ResourceSaver.save(texture_array, "res://texture_2d_array.res", ResourceSaver.FLAG_COMPRESS)
 		
 func getBlockId(block_name: String):
 	return ids[block_name]
